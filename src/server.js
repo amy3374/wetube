af4f37e80877616af9c65886b3fe02d33b39947e
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-04-07 23:14:36 +0900

    first wetube

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const PORT = 4000;
+
+console.log(process.cwd());
+
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT}🚀`);
+
+app.listen(PORT, handleListening);
