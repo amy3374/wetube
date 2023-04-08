b7b7fd9147b9d9c9002c839ae2592d8632699833
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-04-08 23:49:47 +0900

    second

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 export const trending = (req, res) => {
   const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  return res.render("home", { pageTitle: "Home" });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
