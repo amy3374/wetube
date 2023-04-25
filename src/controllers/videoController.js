cb1fe226856263514d02daffa0d0187d6a6b27b5
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-04-25 19:19:28 +0900

    mongoDB만들고 연결

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,60 +1,27 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    Comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    Comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    Comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+import Video from "../models/video";
+
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {});
+  return res.render("home", { pageTitle: "Home" });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching : ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing : ${video.title}`, video });
+  res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Videos" });
 };
 export const postUpload = (req, res) => {
-  const newVideo = {
-    title: req.body.title,
-    rating: 0,
-    Comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
+  const { title } = req.body;
   return res.redirect("/");
 };
