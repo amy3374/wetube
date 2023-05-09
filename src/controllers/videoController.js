127e9db5527b9828dbfb74d73ea3fb12a7cdcc66
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-05-09 19:29:53 +0900

    video upload, db에 저장, edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,17 +1,25 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  return res.render("home", { pageTitle: "Home" });
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  console.log(videos);
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
@@ -19,9 +27,33 @@ export const postEdit = (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
-  return res.render("upload", { pageTitle: "Upload Videos" });
+  return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+  // --> js object를 만들필요 없이 바로 db에 만듦
+  // const video = new Video({
+  //   title,
+  //   description,
+  //   createdAt: Date.now(),
+  //   hashtags: hashtags.split(",").map((word) => `#${word}`),
+  //   meta: {
+  //     views: 0,
+  //     rating: 0,
+  //   },
+  // });
+  // await video.save(); --> js object를 만들고 db에 저장하는 방식
 };
--- src/models/User.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({});
-
-const User = mongoose("User", userSchema);
-export default User;
