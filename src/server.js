df6f9a74503a5f4bd0657ab89825b9be25b6e2ef
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-05-15 23:57:37 +0900

    session

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -12,6 +15,17 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: false, //로그인 한 사용자의 세션만 저장
+    saveUninitialized: false, //세션을 수정할 떄만 세션을 db에 저장학 쿠키 넘겨줌 --> 세션을 언제 수정? 로그인 할  때/ 즉 로그인 한 사용자의 세션만 저장
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
