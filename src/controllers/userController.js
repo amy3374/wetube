631c89070ee58335008dba62def08c769843b372
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-06-03 15:43:47 +0900

    login with  github

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -58,6 +58,18 @@ export const postLogin = async (req, res) => {
   req.session.user = user;
   return res.redirect("/");
 };
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: "77f06d3842f3345495d2",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("Log Out");
--- src/server.js
+++ src/server.js
@@ -18,10 +18,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false, //로그인 한 사용자의 세션만 저장
     saveUninitialized: false, //세션을 수정할 떄만 세션을 db에 저장학 쿠키 넘겨줌 --> 세션을 언제 수정? 로그인 할  때/ 즉 로그인 한 사용자의 세션만 저장
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    // cookie:{
+    //   maxAge:200000, //만료 날짜. 1000이 1초
+    // }
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
